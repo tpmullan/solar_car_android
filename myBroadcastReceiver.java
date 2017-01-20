5fc8caf24dc3e42c37ed613b5789aa237c9e7d72
Michael <mforee@foreeoil.com>
2017-01-19 23:05:41 -0600

    Added some code for broadcast receiver (it probably works!)

--- myBroadcastReceiver.java
+++ myBroadcastReceiver.java
@@ -12,36 +12,33 @@ import android.widget.Toast;
  * Created by Michael on 1/19/2017.
  */
 
-
+/*
+        <receiver android:name="myBroadcastReceiver" >
+            <intent-filter>
+                <action android:name="android.intent.action.BOOT_COMPLETED" />
+                <action android:name="android.net.wifi.supplicant.CONNECTION_CHANGE" />
+                <action android:name="android.net.wifi.STATE_CHANGE" />
+            </intent-filter>
+        </receiver>
+*/
     public class myBroadcastReceiver extends BroadcastReceiver {
 
         @Override
         public void onReceive(Context context, Intent intent) {
-
-            Toast.makeText(context, "wifi 1", Toast.LENGTH_SHORT).show();
-
             context.startService(intent);
-
-
             WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
             if (wifiManager !=null){
-                Toast.makeText(context, "wifi 2", Toast.LENGTH_SHORT).show();
-                //wifiManager.toString();
+                Toast.makeText(context, "wifi not null", Toast.LENGTH_SHORT).show();
             }
             NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-            NetworkInfo info = (NetworkInfo) intent.getParcelableExtra(ConnectivityManager.EXTRA_NETWORK_INFO);
-            info.getType();
             if (networkInfo != null) {
-                Toast.makeText(context, "wifi 3", Toast.LENGTH_SHORT).show();
-                //Log.d(AppConstants.TAG, "Type : " + networkInfo.getType()
-                //      + "State : " + networkInfo.getState());
-
                 if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                    Toast.makeText(context, "wifi 4", Toast.LENGTH_SHORT).show();
-
                     //get the different network states
                     if (networkInfo.getState() == NetworkInfo.State.CONNECTING || networkInfo.getState() == NetworkInfo.State.CONNECTED) {
-                        Toast.makeText(context, "wifi 5", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(context, "wifi connecting", Toast.LENGTH_SHORT).show();
+                    }
+                    if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                        Toast.makeText(context, "wifi connected", Toast.LENGTH_SHORT).show();
                     }
                 }
             }
